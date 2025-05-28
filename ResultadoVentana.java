62b1d263208e38220816c0479da13fd7fbc42250
Ivan140809 <161265841+Ivan140809@users.noreply.github.com>
2025-05-28 12:03:44 -0500

    Create ResultadoVentana.java

---
+++ ResultadoVentana.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ResultadoVentana {
+
+    public static void mostrarResultado(double z) {
+        JFrame frame = new JFrame("Resultado de Evaluación");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 350);
+        frame.setLocationRelativeTo(null);
+
+        JLabel label = new JLabel();
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setVerticalAlignment(JLabel.CENTER);
+        label.setFont(new Font("Arial", Font.BOLD, 20));
+
+        // Elige la imagen y el texto según z
+        String texto;
+        String rutaImg;
+        if (z >= 0.80) {
+            texto = "¡Aceptado!";
+            rutaImg = "feliz.png";
+        } else {
+            texto = "No fue aceptado";
+            rutaImg = "OIP.jpg";
+        }
+
+        // Cargar y escalar la imagen
+        ImageIcon iconoOriginal = new ImageIcon(rutaImg);
+        Image imgEscalada = iconoOriginal.getImage()
+                .getScaledInstance(150, 150, Image.SCALE_SMOOTH);
+        label.setIcon(new ImageIcon(imgEscalada));
+
+        // Configurar posición del texto respecto a la imagen
+        label.setText(texto);
+        label.setHorizontalTextPosition(SwingConstants.CENTER);
+        label.setVerticalTextPosition(SwingConstants.BOTTOM);
+        label.setIconTextGap(15);
+
+        frame.add(label);
+        frame.setVisible(true);
+    }
+}
