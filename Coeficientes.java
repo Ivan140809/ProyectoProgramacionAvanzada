5bb9e4c73176b9c051ba09b11b013242b829db27
Ivan140809 <161265841+Ivan140809@users.noreply.github.com>
2025-05-28 12:04:39 -0500

    Create Coeficientes.java

---
+++ Coeficientes.java
@@ -0,0 +1,18 @@
+import com.google.gson.annotations.SerializedName;
+
+public class Coeficientes {
+    @SerializedName("GRE Score")
+    public double gre;
+    @SerializedName("TOEFL Score")
+    public double toefl;
+    @SerializedName("University Rating")
+    public double rating;
+    @SerializedName("SOP")
+    public double sop;
+    @SerializedName("LOR")
+    public double lor;
+    @SerializedName("CGPA")
+    public double cgpa;
+    @SerializedName("Research")
+    public double research;
+}
